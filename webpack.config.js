9ee823ee74b8803a6a0e3d06cf9b96a61eaf9958
Phil <birkal@outlook.com>
2016-12-23 20:27:45 -0800

    Move dist to public

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,8 @@ var webpack = require('webpack')
 module.exports = {
   entry: './client/main.js',
   output: {
-    path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
+    path: path.resolve(__dirname, './public/dist'),
+    publicPath: '/public/dist/',
     filename: 'build.js'
   },
   module: {
